0db6cf5bed9a1f876f51657cf7a7bcccb9631499
bob <bobmccarthyatx@gmail.com>
2016-10-25 23:40:03 -0500

    added slideshow

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -2,6 +2,7 @@
 
 
 $(document).ready(function() {
+	$('body').show();
     $('#slideshow').cycle({
         fx:     'scrollLeft',
         timeout: 8000,
@@ -43,7 +44,32 @@ $(document).ready(function() {
 	// $('.jsok-placeholder').hide();
 	$('.jsok').show();
 	$('#bnav a').html('');
-	
+
+
+	// bx slider--------
+
+	$('.slider2').bxSlider({
+	slideWidth: 246,
+	minSlides: 2,
+	maxSlides: 2,
+	slideMargin: 20
+	});
+
+	$('.bx-prev').html('<div class="specials-slidecontainer-leftarrow"></div>');
+	$('a.bx-prev').css('text-indent','0px');
+	$('a.bx-prev').css('margin-left','-45px');
+	$('.bx-next').html('<div class="specials-slidecontainer-rightarrow"></div>');
+	$('a.bx-next').css('text-indent','0px');
+	$('a.bx-next').css('margin-right','-50px');
+	$('.bx-pager').hide();
+	$('.bx-viewport').css('box-shadow','none');
+	$('.bx-viewport').css('border','none');
+	$('.bx-viewport').css('height','230px');
+	$('.slide').css('border','1px solid #D0A50A');
+	$('.slide').css('border','1px solid rgba(208,165,10,0.3)');
+	  
+	// end bx slider-----------
+
 
 });
 
